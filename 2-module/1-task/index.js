9e19da6c061f28d225516719a60074d4159060b1
iKopturevskiy <kaping25@gmail.com>
2022-05-29 20:51:40 +0300

    Решил задачи второго модуля.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,26 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
+
+
+function sumSalary(obj) {
+
+  let sum = 0;
+
+  for (let key in obj) {
+
+    if (isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+
+  }
+
+  return sum;
+
 }
