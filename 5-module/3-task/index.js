518c83dcc26d205ada137f831cea6e8c7ea378f0
iKopturevskiy <kaping25@gmail.com>
2022-06-12 23:16:38 +0300

    1. Решение 3 задания 5 модуля. Данное решение не проходит проверку. Однако визуально все работвает. 2. Так же изначально при пуле консоль выдает ошибку: "Refused to apply style from 'http://localhost:63342/assets/styles/button.css' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled." 3. В index.html отсутствовали открывающие теги html и head. Добавил.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  let buttonMoveRight = document.querySelector('.carousel__arrow_right');
+  let buttonMoveLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+
+  buttonMoveLeft.addEventListener('click', moveLeft);
+  buttonMoveRight.addEventListener('click', moveRight);
+
+  let slidePosition = carouselInner.offsetLeft;
+  let slideWidth = carouselInner.offsetWidth;
+
+  function deleteButtonsIf() {
+    if (slidePosition === -(slideWidth * 3)) {
+      buttonMoveRight.style.display = 'none';
+    } else if (slidePosition === 0) {
+      buttonMoveLeft.style.display = 'none';
+    } else {
+      buttonMoveLeft.style.display = '';
+      buttonMoveRight.style.display = '';
+    }
+  }
+
+  deleteButtonsIf();
+
+  function moveLeft() {
+    carouselInner.style.transform = `translateX(${slidePosition += slideWidth}px)`;
+    deleteButtonsIf();
+  }
+
+  function moveRight() {
+    carouselInner.style.transform = `translateX(${slidePosition -= slideWidth}px)`;
+    deleteButtonsIf();
+  }
 }
+
