076b9c985c086b8bd33d2eb9e274e82b4ea5b277
iKopturevskiy <kaping25@gmail.com>
2022-06-12 20:44:45 +0300

    Решение пятого модуля. Задачи №1 и 2.

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let text = document.querySelector('#text');
+  let button = document.querySelector('.toggle-text-button');
+
+  function textOff() {
+    // eslint-disable-next-line no-unused-expressions
+    (text.hidden === true) ? text.hidden = false : text.hidden = true;
+  }
+
+  button.addEventListener('click', textOff);
+
 }
+
