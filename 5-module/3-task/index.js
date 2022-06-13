f56fb503b0c9f65626590c692c2c66801c17a7ea
iKopturevskiy <kaping25@gmail.com>
2022-06-13 12:54:05 +0300

    1. Решение 3 задания 5 модуля:  а) let slidePosition исправлено на 0;  б) (slideWidth * 0) теперь (slideWidth * (carouselInner.children.length - 1))

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,15 +2,26 @@ function initCarousel() {
   let buttonMoveRight = document.querySelector('.carousel__arrow_right');
   let buttonMoveLeft = document.querySelector('.carousel__arrow_left');
   let carouselInner = document.querySelector('.carousel__inner');
+  let slidePosition = 0;
+  let slideWidth = carouselInner.offsetWidth;
+
+  console.log(carouselInner.children.length);
 
   buttonMoveLeft.addEventListener('click', moveLeft);
   buttonMoveRight.addEventListener('click', moveRight);
 
-  let slidePosition = carouselInner.offsetLeft;
-  let slideWidth = carouselInner.offsetWidth;
+  function moveLeft() {
+    carouselInner.style.transform = `translateX(${slidePosition += slideWidth}px)`;
+    deleteButtonsIf();
+  }
+
+  function moveRight() {
+    carouselInner.style.transform = `translateX(${slidePosition -= slideWidth}px)`;
+    deleteButtonsIf();
+  }
 
   function deleteButtonsIf() {
-    if (slidePosition === -(slideWidth * 3)) {
+    if (slidePosition === -(slideWidth * (carouselInner.children.length - 1))) {
       buttonMoveRight.style.display = 'none';
     } else if (slidePosition === 0) {
       buttonMoveLeft.style.display = 'none';
@@ -22,14 +33,6 @@ function initCarousel() {
 
   deleteButtonsIf();
 
-  function moveLeft() {
-    carouselInner.style.transform = `translateX(${slidePosition += slideWidth}px)`;
-    deleteButtonsIf();
-  }
 
-  function moveRight() {
-    carouselInner.style.transform = `translateX(${slidePosition -= slideWidth}px)`;
-    deleteButtonsIf();
-  }
 }
 
