504fc206744be2585993bc95effccbcfb57099d2
iKopturevskiy <kaping25@gmail.com>
2022-06-26 13:53:34 +0300

    Решено задание 1 модуля 7.

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,90 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = this.container();
+    this.ribbonInner = this.elem.querySelector('.ribbon__inner');
+    this.buttonLeft = this.elem.querySelector('.ribbon__arrow_left');
+    this.buttonRight = this.elem.querySelector('.ribbon__arrow_right');
+    this.scrollByRight();
+    this.scrollByLeft();
+    this.ribbonInner.addEventListener('scroll', () => this.hiddenButtons());
+    this.ribbonInner.addEventListener('click', this.newCustomEvent);
+
   }
+
+  container() {
+    return createElement(`
+    <!--Корневой элемент RibbonMenu-->
+  <div class="ribbon">
+
+    <!--Кнопка прокрутки влево-->
+    <button class="ribbon__arrow ribbon__arrow_left">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+
+    <!--Ссылки на категории-->
+    <nav class="ribbon__inner">
+    ${this.categories.map(item => {
+    return `<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`;
+  }).join('')}
+    </nav>
+
+    <!--Кнопка прокрутки вправо-->
+    <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+
+  </div>`);
+  }
+
+  scrollByRight() {
+    this.buttonRight.onclick = () => {
+      this.ribbonInner.scrollBy(350, 0);
+    };
+  }
+
+  scrollByLeft() {
+    this.buttonLeft.onclick = () => {
+      this.ribbonInner.scrollBy(-350, 0);
+    };
+  }
+
+  hiddenButtons () {
+    if (this.ribbonInner.scrollWidth - this.ribbonInner.scrollLeft - this.ribbonInner.clientWidth < 1) {
+      this.buttonRight.classList.remove('ribbon__arrow_visible');
+    } else {
+      this.buttonRight.classList.add('ribbon__arrow_visible');
+    }
+
+    if (this.ribbonInner.scrollLeft === 0) {
+      this.buttonLeft.classList.remove('ribbon__arrow_visible');
+    } else {
+      this.buttonLeft.classList.add('ribbon__arrow_visible');
+    }
+  }
+
+  newCustomEvent (event) {
+    this.target = event.target;
+    this.target.onclick = this.target.preventDefault;
+
+    if (this.target.closest('.ribbon__item')) {
+
+      for (let a of this.target.parentElement.querySelectorAll('.ribbon__item')) {
+
+        if (a.classList.contains('ribbon__item_active')) {
+          a.classList.remove('ribbon__item_active');
+        }
+      }
+    }
+
+    this.target.classList.add('ribbon__item_active');
+
+    const newEvent = new CustomEvent('ribbon-select', {
+      detail: this.target.closest('.ribbon__item').dataset.id,
+      bubbles: true
+    });
+    this.target.dispatchEvent(newEvent);
+  }
+
+
 }
